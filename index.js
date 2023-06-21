0e7073aa667db59b21497c38de8b7c0739908aef
aaryandewan <49852384+aaryandewan@users.noreply.github.com>
2023-06-21 12:28:48 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-class Person{
-  constructor(name){
-    this.name = name;
-  }
-
-  sayName(){
-    console.log(this.name);
-  }
+var once = function (fn) {
+  var flag = 0;
+  return function (...args) {
+    flag++;
+    if (flag == 1) {
+      console.log('z');
+      return fn(...args);
+    }
+    return undefined;
+  };
 };
 
-let p = new Person("aar");
+let fn = (a, b, c) => a + b + c;
+let onceFn = once(fn);
+console.log(onceFn(1, 2, 3)); // 6
+console.log(onceFn(1, 2, 3)); // 6
