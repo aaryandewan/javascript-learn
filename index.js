6279b9aeed2758dddf216efb0a29f17cc1c913bb
aaryandewan <49852384+aaryandewan@users.noreply.github.com>
2023-06-22 10:17:36 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,6 +1,34 @@
-let x = new Map([
-  [(1, 2), 'first'],
-  [(2, 3), 'secon'],
-]);
+function memoize(fn) {
+  let obj = {};
+  return function(...args) {
+      if(args.length == 2){
+          let x = (toString(args[0]) + toString(args[1]));
+          if(obj.hasOwnProperty(x)) return obj.x;
+          else{
+              let ans = fn(...args);
+              obj.x = ans;
+              return ans;
+          }   
+      }
+      else{
+          let x = toString(args[0]);
+          if(obj.hasOwnProperty(x)) return obj.x;
+          else{
+              let ans = fn(...args);
+              obj.x = ans;
+              return ans;
+          }
+      }
+  }
+}
 
-console.log(x.has((9,2)));
+
+ let callCount = 0;
+ const memoizedFn = memoize(function (a, b) {
+	 callCount += 1;
+   return a + b;
+ })
+ memoizedFn(2, 3) // 5
+ memoizedFn(2, 3) // 5
+ console.log(callCount) // 1 
+ 
