b62ba188ec78fc9f93839f6bbe90454409ba5582
aaryandewan <49852384+aaryandewan@users.noreply.github.com>
2023-06-22 10:43:53 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,34 +1,20 @@
 function memoize(fn) {
   let obj = {};
-  return function(...args) {
-      if(args.length == 2){
-          let x = (toString(args[0]) + toString(args[1]));
-          if(obj.hasOwnProperty(x)) return obj.x;
-          else{
-              let ans = fn(...args);
-              obj.x = ans;
-              return ans;
-          }   
-      }
-      else{
-          let x = toString(args[0]);
-          if(obj.hasOwnProperty(x)) return obj.x;
-          else{
-              let ans = fn(...args);
-              obj.x = ans;
-              return ans;
-          }
-      }
-  }
+  console.log(JSON.stringify(arguments));
+  return function (...args) {
+    let x = JSON.stringify(...arguments);
+    if (obj[x]) return obj[x];
+    let ans = fn(...args);
+    obj.x = ans;
+    return ans;
+  };
 }
 
-
- let callCount = 0;
- const memoizedFn = memoize(function (a, b) {
-	 callCount += 1;
-   return a + b;
- })
- memoizedFn(2, 3) // 5
- memoizedFn(2, 3) // 5
- console.log(callCount) // 1 
- 
+let callCount = 0;
+const memoizedFn = memoize(function (a, b) {
+  callCount += 1;
+  return a + b;
+});
+memoizedFn(2, 3); // 5
+memoizedFn(2, 3); // 5
+console.log(callCount); // 1
