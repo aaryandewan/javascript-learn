32ce71b4a20566872da08e96523db10dd2918899
aaryandewan <49852384+aaryandewan@users.noreply.github.com>
2023-06-22 09:50:38 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,16 +1,6 @@
-var once = function (fn) {
-  var flag = 0;
-  return function (...args) {
-    flag++;
-    if (flag == 1) {
-      console.log('z');
-      return fn(...args);
-    }
-    return undefined;
-  };
-};
+let x = new Map([
+  [(1, 2), 'first'],
+  [(2, 3), 'secon'],
+]);
 
-let fn = (a, b, c) => a + b + c;
-let onceFn = once(fn);
-console.log(onceFn(1, 2, 3)); // 6
-console.log(onceFn(1, 2, 3)); // 6
+console.log(x.has((9,2)));
