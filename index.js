891d67f3de39de0d83e7c13de8120565669f3ef2
aaryandewan <49852384+aaryandewan@users.noreply.github.com>
2023-07-10 14:13:18 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
-function memoize(fn) {
-  let obj = {};
-  return function (...args) {
-    let x = JSON.stringify(args); // Corrected line
-    console.log("x", x);
-    if (obj.hasOwnProperty(x)) return obj[x];
-    let ans = fn(...args);
-    obj[x] = ans;
-    console.log(obj);
-    return ans;
-  };
+class Complex {
+  constructor(real, imaginary) {
+    this.r = real;
+    this.i = imaginary;
+  }
+  plus(that) {
+    return new Complex(this.r + that.r, this.i + that.i);
+  }
+
+  static sum(c, d) {
+    return new Complex(c.plus * d);
+  }
+  get real() {
+    return this.r;
+  }
+  get imaginary() {
+    return this.i;
+  }
 }
 
-let callCount = 0;
-const memoizedFn = memoize(function (a, b) {
-  callCount += 1;
-  return a + b;
-});
-memoizedFn(0, 0); // 5
-memoizedFn(0, 0); // 5
-console.log(callCount); // 1
+let one = new Complex(0, 1);
+let two = new Complex(1, 0);
+
+let three = one.plus(two);
+console.log(three.imaginary);
