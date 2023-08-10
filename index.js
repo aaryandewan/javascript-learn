65c4d76a0506fabe849512b9ab9e29321b8568ec
aaryandewan <49852384+aaryandewan@users.noreply.github.com>
2023-08-10 13:09:53 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,25 +1,9 @@
-class Complex {
-  constructor(real, imaginary) {
-    this.r = real;
-    this.i = imaginary;
-  }
-  plus(that) {
-    return new Complex(this.r + that.r, this.i + that.i);
+class A{
+  constructor(nname){
+    this.name = nname;
   }
 
-  static sum(c, d) {
-    return c.plus(d);
-  }
-  get real() {
-    return this.r;
-  }
-  get imaginary() {
-    return this.i;
+  private getName(){
+    console.log(this.name);
   }
 }
-
-let one = new Complex(0, 1);
-let two = new Complex(1, 0);
-
-let three = Complex.sum(one, two);
-console.log(three.imaginary);
